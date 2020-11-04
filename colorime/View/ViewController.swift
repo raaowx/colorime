9732c5a76087aaa80e6a86f5bf768b6eac75df86
raaowx <raaowx@protonmail.com>
2020-11-04 18:28:54 +0100

    [ADD] Xcode Project + SwiftLint + MVP Group Structure + Localization

---
+++ colorime/View/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  colorime
+//
+//  Created by raaowx on 20/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
