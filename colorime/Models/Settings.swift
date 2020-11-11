95082f4216be44044aeda10bc80cbbfaf0de1ad8
raaowx <raaowx@protonmail.com>
2020-11-11 19:17:20 +0100

    [ADD] Settings model

---
+++ colorime/Models/Settings.swift
@@ -0,0 +1,38 @@
+//
+//  Settings.swift
+//  colorime
+//
+//  Created by raaowx on 11/11/2020.
+//
+
+import Foundation
+
+struct Settings {
+  static var shared = Settings()
+  private let defaults: UserDefaults
+
+  enum Options: String {
+    case showTime = "colorime-time-show"
+    case hexTime = "colorime-time-hex"
+    case showDate = "colorime-date-show"
+    case hexDate = "colorime-date-hex"
+    case keepScreenActive = "colorime-screen-keepactive"
+  }
+
+  private init() {
+    defaults = UserDefaults.standard
+  }
+
+  func read(option: Options) -> Bool {
+    switch option {
+    case .showTime:
+      return true
+    default:
+      return defaults.bool(forKey: option.rawValue)
+    }
+  }
+
+  func write(option: Options, withValue value: Bool) {
+    defaults.setValue(value, forKey: option.rawValue)
+  }
+}
